6e113fc64720be24f86cf209d268352a0f16986a
nolexis <alexn4999@gmail.com>
2020-02-25 12:03:58 +0700

    Добавлю комментарий

--- ContactsApp/MainForm.cs
+++ ContactsApp/MainForm.cs
@@ -11,7 +11,7 @@ using System.Windows.Forms;
 namespace ContactsApp
 {
     public partial class MainForm : Form
-    {
+    {//sfdlklvdwp
         public MainForm()
         {
             InitializeComponent();
